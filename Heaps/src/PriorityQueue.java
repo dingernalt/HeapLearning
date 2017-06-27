085001512f67c6d983420a78460b025398f5b659
14110462 <14110462@W3331980.portoalegre.pucrsnet.br>
2017-06-27 11:26:33 -0300

    Aula

--- Heaps/src/PriorityQueue.java
+++ Heaps/src/PriorityQueue.java
@@ -1,13 +1,22 @@
 
 public class PriorityQueue {
 	private int[] array;
-	
+	private Heap heap;
 	public PriorityQueue(int[] array) {
-		this.array = array; 
+		this.array = array;
+		heap.buildMaxHeap(array, array.length);
 	}
 	
 	public PriorityQueue() {
 		
+	}
+	
+	public void offer(int n) {
+		int[] aux = new int[array.length+1];
+		for(int i = 0; i < array.length; i++) { aux[i] = array[i]; }
+		aux[array.length] = n;
+		heap.revMaxHeapify(aux);
+		array = aux;
 	}
 	
 	public int poll() {
